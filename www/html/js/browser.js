28ade1be32eb2027df644b73bdacc60c1df988d0
John Nelson <me@johnrnelson.com>
2016-03-21 21:02:52 +0000

    Updated by John Nelson. Too lazy to document why. :-)

--- www/html/js/browser.js
+++ www/html/js/browser.js
@@ -13,52 +13,65 @@ var WebApp = {
     ReadFile: function(FileObject) {
         // console.log('FileObject', FileObject);
         WebApp.Elements.MovieDisplay.innerHTML = '';
-        
-        if(!WebApp.Elements.FileListDisplay.files){
+
+        if (!WebApp.Elements.FileListDisplay.files) {
             WebApp.Elements.FileListDisplay.files = {};
         }
-        
+
         var fileElement = document.createElement('div');
-        
+
         var fileInfo = document.createElement('span');
+        var fileIconUpload = document.createElement('i');
+
+
         var fileVideo = document.createElement('video');
-        
-        fileVideo.setAttribute('controls','true');
-        
+
+        fileVideo.setAttribute('controls', 'true');
+
         // fileVideo.style.height='100%';
-        fileVideo.style.height='150px';
-        fileVideo.style.width='100%';
-        
-        
-        fileElement.className="LocalFileDisplay";
-        fileInfo.className="LocalFileDisplayInfo";
-        fileVideo.className="LocalFileDisplayVideo";
-        
-        
+        fileVideo.style.height = '150px';
+        fileVideo.style.width = '100%';
+
+
+        fileElement.className = "LocalFileDisplay";
+        fileInfo.className = "LocalFileDisplayInfo";
+        fileVideo.className = "LocalFileDisplayVideo";
+
+
+        fileElement.FileIconUpload = fileElement.appendChild(fileIconUpload);
         fileElement.FileInfo = fileElement.appendChild(fileInfo);
         fileElement.FileVideo = fileElement.appendChild(fileVideo);
-        
-        
+
+
         var FileRecord = {
-            name:FileObject.name,
-            ext:FileObject.name.split('.').pop().toLowerCase(),
-            file:FileObject,
-            element:fileElement
+            name: FileObject.name,
+            ext: FileObject.name.split('.').pop().toLowerCase(),
+            file: FileObject,
+            element: fileElement
         };
-        
+
+
+        fileIconUpload.className = "fa fa-cloud-upload";
+        fileIconUpload.filename = FileRecord.name;
+        fileIconUpload.onclick = function() {
+            var FileRecord = WebApp.Elements.FileListDisplay.files[this.filename];
+            WebApp.UploadFile(FileRecord);
+        }
+
+
         fileInfo.innerHTML = FileRecord.name;
-        
+
         //Keep track of the files we are using by doing a memory map...
         WebApp.Elements.FileListDisplay.files[FileObject.name] = FileRecord;
-        
-        
+
+
         //Tell the UI we have a brand spanking new HTML element for it to display..
         WebApp.Elements.FileListDisplay.appendChild(FileRecord.element);
-        
-        
-        
+
+
+
         //Rather than read the file to make sure it's video, we just use the file extension...
-        if(FileRecord.ext!='mp4'){
+        if (FileRecord.ext != 'mp4') {
             // debugger;
             alert('We are only doing MP4 files for now.');
             return;
@@ -72,68 +85,73 @@ var WebApp = {
             var doh = this.FileRecord;
             console.log(FileRecord.element.FileVideo)
             console.log(FileRecord.element)
-            // debugger
+                // debugger
             FileRecord.element.FileVideo.setAttribute("src", event.target.result);
             // WebApp.Elements.MovieElement.setAttribute("src", event.target.result);
         };
         fileReader.readAsDataURL(FileObject);
     },
-    UploadFile: function() {
+    UploadFile: function(FileRecord) {
+
+        // debugger;
         /*
             Upload the active file by converting into an array and sending 
             it out a new connection to the server...
         */
-        if (WebApp.ActiveFile == null) {
-            WebApp.Elements.MovieDisplay.innerHTML = 'Select a file using the Folder Open icon or drag a file into this window.';
-        }
-        else {
-            var oReq = new XMLHttpRequest();
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                oReq.open("POST", '/' + WebApp.ActiveFile.name, true);
-                //We use headers to tell the server what to do. This way we don't mess up the body...
-                oReq.setRequestHeader("route", "upload");
+        var oReq = new XMLHttpRequest();
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            oReq.open("POST", '/' + FileRecord.name, true);
+            //We use headers to tell the server what to do. This way we don't mess up the body...
+            oReq.setRequestHeader("route", "upload");
 
-                oReq.onload = function(oEvent) {
-                    // Uploaded.
-                    debugger;
-                    var resMSG = JSON.parse(oReq.responseText);
-                    if(resMSG.errmsg!=""){
-                        alert(resMSG.errmsg)
-                    }else{
-                        WebApp.GetList();
-                        
-                    }
-                };
+            oReq.onload = function(oEvent) {
+                var resMSG = JSON.parse(oReq.responseText);
+                if (resMSG.errmsg != "") {
+                    alert(resMSG.errmsg);
+                }
+                else {
 
-                var uInt8Array = new Uint8Array(e.target.result);
-                oReq.send(uInt8Array.buffer);
+                    console.log(FileRecord.element);
+                    console.log(WebApp.Elements.FileListDisplay.files);
+                    var sFileRecord = FileRecord;
+                    // debugger;
+                    FileRecord.element.parentElement.remove(FileRecord.element);
+                    delete WebApp.Elements.FileListDisplay.files[FileRecord.name]
+
+                    WebApp.GetList();
+
+                }
             };
-            reader.onerror = function(e) {
-                debugger;
-                console.error(e);
-            };
-            reader.readAsArrayBuffer(WebApp.ActiveFile);
-        }
+
+            var uInt8Array = new Uint8Array(e.target.result);
+            oReq.send(uInt8Array.buffer);
+        };
+        reader.onerror = function(e) {
+            debugger;
+            console.error(e);
+        };
+        reader.readAsArrayBuffer(FileRecord.file);
+
     },
     GetList: function() {
         var oReq = new XMLHttpRequest();
         oReq.open("GET", '/', true);
         //We use headers to tell the server what to do. This way we don't mess up the body...
         oReq.setRequestHeader("route", "list");
-        
+
         WebApp.Elements.MovieListDisplay.innerHTML = ""; //clear out any old elements...
 
         oReq.onload = function(oEvent) {
             var files = JSON.parse(oReq.responseText);
-            
-            for (var f = files.length; f--; ) {
+
+            for (var f = files.length; f--;) {
                 var file = files[f];
                 var fileElement = document.createElement('div');
                 fileElement.FileName = file;
                 fileElement.innerHTML = fileElement.FileName;
-                fileElement.onclick = function(e){
-                    WebApp.Elements.MovieElement.setAttribute('src','/uploads/'+this.FileName);
+                fileElement.onclick = function(e) {
+                    WebApp.Elements.MovieElement.setAttribute('src', '/uploads/' + this.FileName);
                 }
                 WebApp.Elements.MovieListDisplay.appendChild(fileElement);
             }
